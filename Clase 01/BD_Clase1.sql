abbc13ec473a21afbf450bde8fa27d73705e1912
Angel <asimon@docentes.frgp.utn.edu.ar>
2021-08-02 20:12:44 -0300

    Archivos - Clase 1

---
+++ Clase 01/BD_Clase1.sql
@@ -0,0 +1,25 @@
+Create Database Clase1
+go
+Use Clase1
+go
+Create Table Areas(
+    ID bigint not null PRIMARY KEY,
+    Nombre varchar(50) not null,
+    Presupuesto money not null check(Presupuesto > 0),
+    Mail varchar(100) not null unique
+)
+go
+Create Table Empleados(
+    Legajo bigint not null primary key identity(100, 10),
+    IDArea bigint not null foreign key references Areas(ID),
+    Apellidos varchar(100) not null,
+    Nombres varchar(100) not null,
+    Nacimiento date not null,
+    Telefono varchar(20) null, -- Acepta este formto: +54011154567989
+    Sueldo money not null check (Sueldo >= 0)
+)
+
+/*
+    Comentario de
+    muchas líneas.
+*/
